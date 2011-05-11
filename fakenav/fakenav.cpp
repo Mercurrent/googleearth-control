81546ad759a852d6107c60defb097c2188c3644f
Paul Hunkin <paul@bieh.net>
2011-05-12 00:13:15 +1200

    Formatting

--- fakenav/fakenav.cpp
+++ fakenav/fakenav.cpp
@@ -9,7 +9,7 @@
 
 //axis values
 float x, y, z, yaw, pitch, roll = 0.0f;
-	
+    
 //the named pipe
 unsigned int fd = 0;
 
@@ -19,56 +19,56 @@ std::vector<int> handles;
 
 //write a single axis input_event
 void write_axis(int axid, float amount){
-	struct input_event ev;
+    struct input_event ev;
 
-	ev.code = axid;
-	ev.value = amount;
-	ev.type = EV_REL;
-	
-	gettimeofday(&ev.time, NULL);
+    ev.code = axid;
+    ev.value = amount;
+    ev.type = EV_REL;
+    
+    gettimeofday(&ev.time, NULL);
 
-	write(fd, &ev, sizeof(ev));
+    write(fd, &ev, sizeof(ev));
 }
 
 void update_dev_file(){
-	write_axis(0, x);
-	write_axis(1, y);
-	write_axis(2, z);
-	write_axis(3, pitch);
-	write_axis(4, roll);
-	write_axis(5, yaw);
+    write_axis(0, x);
+    write_axis(1, y);
+    write_axis(2, z);
+    write_axis(3, pitch);
+    write_axis(4, roll);
+    write_axis(5, yaw);
 }
 
 //main fork()'ing socket server
 int run_server(){
-	int serversock, portno;	
-	
-	socklen_t clilen;
-	serversock = socket(AF_INET, SOCK_STREAM, 0);
+    int serversock, portno;    
+    
+    socklen_t clilen;
+    serversock = socket(AF_INET, SOCK_STREAM, 0);
 
-	if (serversock < 0){
-		printf("ERROR opening socket");
-		return -1;
-	}
-	int optval = 1;
+    if (serversock < 0){
+        printf("ERROR opening socket");
+        return -1;
+    }
+    int optval = 1;
     setsockopt(serversock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
 
-	struct sockaddr_in serv_addr, cli_addr;
+    struct sockaddr_in serv_addr, cli_addr;
 
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	portno = SERVER_PORT;
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons(portno);
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = SERVER_PORT;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
 
-	if (bind(serversock, (struct sockaddr *) &serv_addr,
-			sizeof(serv_addr)) < 0){
-		printf("ERROR on binding");
-		return -1;
-	}
+    if (bind(serversock, (struct sockaddr *) &serv_addr,
+            sizeof(serv_addr)) < 0){
+        printf("ERROR on binding");
+        return -1;
+    }
 
-	listen(serversock, 5);
-		        
+    listen(serversock, 5);
+                
     printf("waiting for clients\n");
     
     clilen = sizeof(cli_addr);
@@ -91,22 +91,22 @@ int run_server(){
             printf("Client connected\n");
             
             while(true){                                    
-	            if(read(newsockfd, buffer, 256) <= 0){
-	                printf("client disconnected\n");
-	                exit(0);
-	            }
-	            
-	            //printf("Got: %s\n", buffer);
-	             	            
-	            if(sscanf(buffer, "%f, %f, %f, %f, %f, %f\n", 
-			            &x, &y, &z, &yaw, &pitch, &roll) == 6){	            
-                    update_dev_file();
-	            }else{
-	                printf("Couldn't parse: '%s'\n", buffer);
+                if(read(newsockfd, buffer, 256) <= 0){
+                    printf("client disconnected\n");
+                    exit(0);
                 }
-	            
+                
+                //printf("Got: %s\n", buffer);
+                                 
+                if(sscanf(buffer, "%f, %f, %f, %f, %f, %f\n", 
+                        &x, &y, &z, &yaw, &pitch, &roll) == 6){                
+                    update_dev_file();
+                }else{
+                    printf("Couldn't parse: '%s'\n", buffer);
+                }
+                
             }
-	       
+           
             
         }else{
         
@@ -131,14 +131,14 @@ void on_sigint(int sig) {
 //entry point
 int main(int argc, char **argv){
 
-	printf("fakenav!\n");
-	printf("waiting for google earth to open %s...\n", DEV_NAME);
+    printf("fakenav!\n");
+    printf("waiting for google earth to open %s...\n", DEV_NAME);
 
-	fd = open(DEV_NAME, O_WRONLY);
-		
-	printf("opened pipe!\n");
-	
-	signal(SIGINT, on_sigint);
-		
-	run_server();
+    fd = open(DEV_NAME, O_WRONLY);
+        
+    printf("opened pipe!\n");
+    
+    signal(SIGINT, on_sigint);
+        
+    run_server();
 }
